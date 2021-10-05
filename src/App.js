0b37c65c179066af9663f82c506927cf4a33e3ce
humonnom <josojoso0702@gmail.com>
2021-10-06 00:46:07 +0900

    feat: add redux

--- src/App.js
+++ src/App.js
@@ -6,60 +6,31 @@ import {
   EditModePage,
   LoginPage,
   KakaoCallback,
+  Main,
 } from './pages';
-import { SERVER_URI, REFRESH_TOKEN } from './utils/constantValue';
+
+// require('dotenv').config();
 
 function App() {
-  async function renewAccessToken() {
-    const fetchedData = await fetch(`${SERVER_URI}/token/refresh`, {
-      method: 'GET',
-      headers: {
-        refresh_token: `Bearer ${REFRESH_TOKEN}`,
-      },
-    })
-      .then(async (res) => {
-        const result = await res.json();
-        console.log('result:');
-        console.log(result);
-        const ACCESS_TOKEN = result.data.access_token;
-        console.log('ACCESS_TOKEN renew:');
-        console.log(ACCESS_TOKEN);
-        // 로컬에 저장함(약속된 사항)
-        localStorage.setItem('access_token', ACCESS_TOKEN);
-        window.location.assign('/');
-        return result;
-        // history.goBack();
-      })
-      .then((data) => data)
-      .catch((error) => console.log('error'));
-    console.log(JSON.stringify(fetchedData));
-  }
   return (
     <PageWrapper>
       <Router>
         <Switch>
           <Route exact path='/'>
-            <div>
-              <button
-                type='button'
-                onClick={() => window.location.assign('/normal')}
-              >
-                my homepage
-              </button>
-              <button
-                type='button'
-                onClick={() => window.location.assign('/login')}
-              >
-                login
-              </button>
-              <button type='button' onClick={renewAccessToken}>
-                토큰 재발급
-              </button>
-            </div>
+            <Main />
           </Route>
           <Route exact path='/callback/kakao'>
             <KakaoCallback />
           </Route>
+          {/* <Route exact path='/redux'>
+            <div>
+              <h3>{reduxTestString}</h3>
+              <input type='input' onChange={}/>
+              <button type='button' onClick={handleClick}>
+                save
+              </button>
+            </div>
+          </Route> */}
           <Route exact path='/login'>
             <LoginPage />
           </Route>
--- src/pages/EditModePage.js
+++ src/pages/EditModePage.js
@@ -1,28 +1,62 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   PageWrapper,
   EditWrapper,
   ToolBar,
   EditAllWidgets,
 } from '../components';
+import { createReplacementWidgetAction } from '../redux/slice';
 
 function EditMode(props) {
-  const [layoutInfo, setLayoutInfo] = useState([
-    {
-      i: '0',
-      x: 1,
-      y: 1,
-      w: 2,
-      h: 2,
-      type: 'txt',
-      source: '블록을 추가하세요!',
-    },
-  ]);
+  // redux test
+  const dispatch = useDispatch();
+  const { widget } = useSelector((state) => ({
+    widget: state.info.widget,
+  }));
+  const layoutInfo = [widget];
+
+  // const updateSize = (newW, newH) => {
+  //   dispatch(
+  //     createReplacementWidgetAction({
+  //       ...widget,
+  //       w: newW,
+  //       h: newH,
+  //     })
+  //   );
+  // };
+  const updatePos = (newX, newY) => {
+    dispatch(
+      createReplacementWidgetAction({
+        ...widget,
+        y: newY,
+        x: newX,
+      })
+    );
+  };
+  const handlePosUpdate = ({ i, x, y, w, h }) => {
+    console.log('change');
+    updatePos(x, y);
+    console.log(x);
+    console.log(y);
+    console.log(widget.x);
+    console.log(widget.y);
+  };
+  // const [layoutInfo, setLayoutInfo] = useState([
+  //   {
+  //     i: '0',
+  //     x: 1,
+  //     y: 1,
+  //     w: 2,
+  //     h: 2,
+  //     type: 'txt',
+  //     source: '블록을 추가하세요!',
+  //   },
+  // ]);
   const [isPop, setIsPop] = useState({
     on: 0,
     type: 'none',
   });
-
   return (
     <PageWrapper>
       <EditWrapper>
@@ -32,7 +66,7 @@ function EditMode(props) {
             console.log('onDragOver');
             // console.log(event);
           }}
-          layoutInfo={layoutInfo}
+          // layoutInfo={layoutInfo}
           isPop={isPop}
         />
 
@@ -43,10 +77,13 @@ function EditMode(props) {
               // console.log(event);
 
               // TODO: Calculate coordinates using screen coordinates
-              setLayoutInfo([
-                ...layoutInfo,
-                { i: layoutInfo.length, x: 12, y: 6, w: 4, h: 4 },
-              ]);
+              handlePosUpdate({
+                i: layoutInfo.length,
+                x: 12,
+                y: 6,
+                w: 4,
+                h: 4,
+              });
               setIsPop({
                 on: 0,
                 type: 'none',
