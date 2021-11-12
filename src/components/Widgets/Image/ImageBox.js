d4961a9cfce6af3cc021730f79fab2032135c332
humonnom <josojoso0702@gmail.com>
2021-11-12 12:35:36 +0900

    feat: 링크 있는 이미지 클릭시 새탭으로 링크열기 구현	- 프론트엔드에서 사용하던 데이터 구조 수정 (url -> thumbnail, link -> url)

--- src/components/Widgets/Image/ImageBox.js
+++ src/components/Widgets/Image/ImageBox.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { css } from '@emotion/css';
 
 export default function ImageBox({ element }) {
   console.log('이미지 박스');
+  const [hasLink, setHasLink] = useState(false);
   // TODO: 서버랑 맞추고 삭제할 부분
   const defaultImg =
     'https://www.thewindowsclub.com/wp-content/uploads/2018/06/Broken-image-icon-in-Chrome.gif';
-  const img_src = element.widget_data.url || defaultImg;
+  const img_src = element.widget_data.thumbnail || defaultImg;
 
   // console.log(img_src);
   //
+  useEffect(() => {
+    if (element.widget_data.url !== null) {
+      setHasLink(true);
+      console.log('has link');
+    }
+  }, []);
 
   return (
     <>
-      <img src={img_src} alt='profile' style={imgStyle} />
-      {/* <img src={element.widget_data.url} alt='profile' style={imgStyle} /> */}
+      {hasLink && (
+        <a
+          href={element.widget_data.url}
+          target='_blank'
+          rel='noreferrer'
+          className={css`
+            background-color: red;
+            display: inline-block;
+            width: 100%;
+            height: 100%;
+          `}
+        >
+          <img src={img_src} alt='profile' style={imgStyle} />
+        </a>
+      )}
+      {!hasLink && <img src={img_src} alt='profile' style={imgStyle} />}
     </>
   );
 }
