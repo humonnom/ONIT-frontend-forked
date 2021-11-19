321af85a7c7476a9823b7167eb7d6e7068c7e1e2
humonnom <josojoso0702@gmail.com>
2021-11-19 11:28:15 +0900

    feat: 로그아웃 기능 구현

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import RenderLoginPage from './RenderLoginPage';
 
 function LoginPage() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  if (loggedIn) {
+    window.location.assign('/main');
+  }
+
+  useEffect(() => {
+    const access_token = localStorage.getItem('access_token');
+    if (access_token) {
+      setLoggedIn(true);
+    }
+  }, []);
+
   return <RenderLoginPage />;
 }
 
