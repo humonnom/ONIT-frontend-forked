fe052e652136aa129ce9a7ec970bd2cfa56cdb1b
humonnom <josojoso0702@gmail.com>
2022-01-29 23:53:53 +0900

    feat: nanoid 설치제거 	- 사용자에게 url를 필수로 입력하도록 하고 랜덤 url을 삭제함

--- src/pages/JoinPage.js
+++ src/pages/JoinPage.js
@@ -4,19 +4,25 @@ import { css } from '@emotion/react';
 import { useHistory } from 'react-router';
 
 import { isEmail, isURL } from 'validator';
-import { nanoid } from 'nanoid';
 import { isPassword } from '../utils/util';
 
 // [ state ]
-// 0. init : 아직 입력 시작하지 않음 -> 에러 띄우지 않음
-// 1. empty : 비어있는 상태 -> 에러 메세지 띄우기 시작
-// 2. invalid : 조건에 부합하지 않음
-// invalid_dup => 중복
-// invalid_len_short, invalid_len_high => 길이제한 위반
-// invalid_form => 형식위반
+// 1. empty : 비어있는 상태 -> 아직 입력 전이므로 가이드 메세지 띄우지 않음
+// 2. invalid_{detail} : 조건에 부합하지 않음 -> 가이드 메세지 띄움
+//    invalid_dup                           >> 중복
+//    invalid_len_short, invalid_len_high   >> 길이제한 위반
+//    invalid_form                          >> 형식위반
+//    invalid                               >> 그 외 위반사항
 // 3. valid : 제출 가능
-// url은 제출하지않으면 랜덤으로 생성된 string으로 적용됨
-// TODO: 중복체크 필요한 항목: url, nickname
+
+// [ 특이사항 ]
+// url을 제출하지않으면 랜덤 string으로 적용
+// 랜덤 string: nanoid 패키지로 생성
+
+// [ TODO ]
+// 중복체크 필요한 항목: url, nickname
+// 태킴님께 에러메세지 뜨는 순간이 좋을지 여쭤보기, 체크박스
+// 분야선택 안해도 가입 시키기!!!
 
 const postData = (data) => {
   console.log('[submited]');
@@ -59,7 +65,6 @@ function JoinPage() {
   // TODO: 분야선택도 조건에 넣기
   const history = useHistory();
   const fieldList = getFieldList();
-  const randomUrl = useMemo(() => nanoid(), []);
 
   const passwordState = useMemo(() => {
     // 영문, 숫자, 특수문자
@@ -79,6 +84,7 @@ function JoinPage() {
   const urlState = useMemo(() => {
     const formedUrl = `http://${url}.kr`;
     if (url.length === 0) return 'empty';
+    // else if (중복체크state) return 'invalid_dup';
     else if (!isURL(formedUrl)) return 'invalid_form';
     else if (url.length < 4) return 'invalid_len_short';
     else if (url.length > 20) return 'invalid_len_long';
@@ -86,8 +92,9 @@ function JoinPage() {
   }, [url]);
 
   const fieldState = useMemo(() => {
-    if (field.length >= 1) return 'valid';
-    else return 'invalid';
+    // if (field.length >= 1) return 'valid';
+    // else return 'invalid';
+    return 'valid';
   }, [field]);
 
   const agreementState = useMemo(() => {
@@ -96,9 +103,8 @@ function JoinPage() {
   }, [agreement]);
 
   const nameState = useMemo(() => {
-    // 허용: 영문, 한글, 숫자
-    // 길이 1-15
     if (name.length === 0) return 'empty';
+    // else if (중복체크state) return 'invalid_dup';
     else if (name.length > 15) return 'invalid_len_long';
     else return 'valid';
   }, [name]);
@@ -111,7 +117,7 @@ function JoinPage() {
       // isValid(fieldState) &&
       isValid(passwordState) &&
       isValid(emailState) &&
-      (isValid(urlState) || urlState === 'empty') &&
+      isValid(urlState) &&
       isValid(nameState)
     ) {
       return false;
@@ -164,7 +170,7 @@ function JoinPage() {
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
-    if (agreementState === 'invalid' || fieldState === 'invalid') {
+    if (agreementState === 'invalid') {
       setSubmitTried(true);
     } else {
       postData({
@@ -204,99 +210,141 @@ function JoinPage() {
   );
 
   return (
-    <div css={wrapper}>
+    <div>
       <button type='button' onClick={() => history.goBack()}>
         되돌아가기 버튼
       </button>
-      <form css={formStyle} onSubmit={onSubmitHandler}>
-        <div css={inputUnitStyle}>
-          <label css={labelStyle} htmlFor='email'>
-            이메일
-          </label>
-          <input
-            id='email'
-            type='email'
-            value={email}
-            onChange={(event) => setEmail(event.currentTarget.value)}
-          />
-          <p css={[guideMessageStyle]}>{emailGuide}</p>
-        </div>
-        <div css={inputUnitStyle}>
-          <label css={labelStyle} htmlFor='password'>
-            비밀번호
-          </label>
-
-          <input
-            id='password'
-            type={showPassword ? 'text' : 'password'}
-            value={password}
-            onChange={(event) => setPassword(event.currentTarget.value)}
-          />
-          <button type='button' onClick={() => setShowPassword(!showPassword)}>
-            {showPassword ? '비밀번호 숨기기' : '비밀번호 보이기'}
-          </button>
-          <p css={[guideMessageStyle]}>{passwordGuide}</p>
-        </div>
-        <div css={inputUnitStyle}>
-          <label css={labelStyle} htmlFor='nickname'>
-            닉네임
-          </label>
-          <input
-            id='nickname'
-            type='text'
-            value={name}
-            onChange={(event) => setName(event.currentTarget.value)}
-          />
-          <p css={[guideMessageStyle]}>{nameGuide}</p>
-        </div>
-        <div css={inputUnitStyle}>
-          <label css={labelStyle} htmlFor='url'>
-            개인 url
-          </label>
-          <div css={flexLow}>
-            <p
-              style={{
-                margin: '6px',
-              }}
-            >
-              iamonit.kr/
-            </p>
+      <div>
+        <form css={[formStyle]} onSubmit={onSubmitHandler}>
+          <div css={[inputUnitStyle, emailWrapper]}>
+            <label css={labelStyle} htmlFor='email'>
+              이메일
+            </label>
             <input
-              id='url'
-              type='text'
-              value={url}
-              onChange={(event) => setUrl(event.currentTarget.value)}
-              placeholder={randomUrl}
+              id='email'
+              type='email'
+              value={email}
+              onChange={(event) => setEmail(event.currentTarget.value)}
             />
+            <p css={[guideMessageStyle]}>{emailGuide}</p>
           </div>
-          <p css={[guideMessageStyle]}>{urlGuide}</p>
-        </div>
-        <div css={inputUnitStyle}>
-          <label htmlFor='field'>분야 선택 (하나 이상 선택)</label>
-          <div>{fieldButtons}</div>
-          <p css={[guideMessageStyle]}>{fieldGuide}</p>
-        </div>
-        <div css={inputUnitStyle}>
-          <input
-            type='checkbox'
-            id='agreement'
-            onChange={(event) => setAgreement(event.target.checked)}
-          />
-          <label css={labelStyle} htmlFor='agreement'>
-            약관에 동의합니다.
-          </label>
-          <p css={[guideMessageStyle]}>{agreementGuide}</p>
-        </div>
-        <br />
-        <button type='submit' disabled={disableSubmit}>
-          생성 완료
-        </button>
-      </form>
+          <div css={[inputUnitStyle, passwordWrapper]}>
+            <label css={labelStyle} htmlFor='password'>
+              비밀번호
+            </label>
+
+            <input
+              id='password'
+              type={showPassword ? 'text' : 'password'}
+              value={password}
+              onChange={(event) => setPassword(event.currentTarget.value)}
+            />
+            <button
+              type='button'
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? '비밀번호 숨기기' : '비밀번호 보이기'}
+            </button>
+            <p css={[guideMessageStyle]}>{passwordGuide}</p>
+          </div>
+          <div css={[inputUnitStyle, nameWrapper]}>
+            <label css={labelStyle} htmlFor='nickname'>
+              닉네임
+            </label>
+            <input
+              id='nickname'
+              type='text'
+              value={name}
+              onChange={(event) => setName(event.currentTarget.value)}
+            />
+            <p css={[guideMessageStyle]}>{nameGuide}</p>
+          </div>
+          <div css={[inputUnitStyle, urlWrapper]}>
+            <label css={labelStyle} htmlFor='url'>
+              개인 url
+            </label>
+            <div css={flexLow}>
+              <p
+                style={{
+                  margin: '6px',
+                }}
+              >
+                iamonit.kr/
+              </p>
+              <input
+                id='url'
+                type='text'
+                value={url}
+                onChange={(event) => setUrl(event.currentTarget.value)}
+                // placeholder={url}
+              />
+            </div>
+            <p css={[guideMessageStyle]}>{urlGuide}</p>
+          </div>
+          <div css={[inputUnitStyle, fieldWrapper]}>
+            <label css={labelStyle} htmlFor='field'>
+              분야 선택 (회원가입 후에도 추가할 수 있습니다)
+            </label>
+            <div>{fieldButtons}</div>
+            <p css={[guideMessageStyle]}>{fieldGuide}</p>
+          </div>
+          <div css={inputUnitStyle}>
+            <input
+              type='checkbox'
+              id='agreement'
+              onChange={(event) => setAgreement(event.target.checked)}
+            />
+            <label htmlFor='agreement'>약관에 동의합니다.</label>
+            <p css={[guideMessageStyle]}>{agreementGuide}</p>
+          </div>
+          <br />
+          <button type='submit' disabled={disableSubmit}>
+            생성 완료
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
 
 export default JoinPage;
+
+const formStyle = css`
+  background-color: yellow;
+  margin: 10px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+  grid-auto-rows: minmax(50px, auto);
+`;
+
+const emailWrapper = css`
+  grid-column: 1 / 4;
+  grid-row: 1;
+`;
+const passwordWrapper = css`
+  grid-column: 1 / 4;
+  grid-row: 2;
+`;
+const nameWrapper = css`
+  grid-column: 1 / 4;
+  grid-row: 3;
+`;
+const urlWrapper = css`
+  grid-column: 1 / 4;
+  grid-row: 4;
+`;
+const fieldWrapper = css`
+  grid-column: 1 / 4;
+  grid-row: 5 / 7;
+`;
+
+const inputUnitStyle = css`
+  background-color: #e8e8e8;
+  padding: 15px 0px;
+  margin: 3px 0px;
+`;
+
 const clickedButton = css`
   background-color: gray;
   color: white;
@@ -317,6 +365,7 @@ function getColorByState(field, id) {
 const fieldButton = css`
   padding: 10px;
   margin: 10px;
+  min-width: 100px;
   background-color: white;
   &:active {
     background-color: black;
@@ -324,15 +373,8 @@ const fieldButton = css`
   }
 `;
 
-const wrapper = css`
-  margin: 30px;
-
-  input:invalid {
-    border: 2px solid red;
-  }
-`;
-
 const labelStyle = css`
+  ${'' /* background-color: red; */}
   color: gray;
 `;
 
@@ -345,14 +387,4 @@ const guideMessageStyle = css`
   color: red;
   font-size: 12px;
   margin: 5px 0;
-`;
-
-const formStyle = css`
-  display: flex;
-  flex-direction: column;
-`;
-const inputUnitStyle = css`
-  background-color: yellow;
-  padding: 15px 0px;
-  margin: 3px 0px;
 `;
