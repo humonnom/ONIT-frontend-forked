0209dbfe7c0cb4f66f747675160d64cdac0c1628
humonnom <josojoso0702@gmail.com>
2022-02-04 09:56:59 +0900

    feat: JoinPage 리팩토링 	- useInput 만들어서 input에 필요한 데이터를 반환하도록 함

--- src/pages/JoinPage.js
+++ src/pages/JoinPage.js
@@ -2,9 +2,6 @@
 import { useCallback, useMemo, useState } from 'react';
 import { css } from '@emotion/react';
 import { useHistory } from 'react-router';
-
-import { isEmail, isURL } from 'validator';
-import { isPassword } from '../utils/util';
 import {
   COLOR_STYLE,
   FlexColCenter,
@@ -13,39 +10,7 @@ import {
   FlexSpaceBetweenCenter,
   FlexSpaceBetweenStart,
 } from '../styles/GlobalStyles';
-
-// [ state ]
-// 1. empty : 비어있는 상태 -> 아직 입력 전이므로 가이드 메세지 띄우지 않음
-// 2. invalid_{detail} : 조건에 부합하지 않음 -> 가이드 메세지 띄움
-//    invalid_dup                           >> 중복
-//    invalid_len_short, invalid_len_high   >> 길이제한 위반
-//    invalid_form                          >> 형식위반
-//    invalid                               >> 그 외 위반사항
-// 3. valid : 제출 가능
-
-// [ 특이사항 ]
-// url을 제출하지않으면 랜덤 string으로 적용
-// 랜덤 string: nanoid 패키지로 생성
-
-// [ TODO ]
-// 중복체크 필요한 항목: url, nickname
-// 태킴님께 에러메세지 뜨는 순간이 좋을지 여쭤보기, 체크박스
-// 분야선택 안해도 가입 시키기!!!
-
-const postData = (data) => {
-  console.log('[submited]');
-  console.log(data);
-  // fetch(`http://localhost:8080/auth/join/local`, {
-  //   method: 'POST',
-  //   headers: {
-  //     Accept: 'application/json',
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify(data),
-  // }).then((response) => {
-  //   console.log(response);
-  // });
-};
+import { useInput } from '../hooks/useInput';
 
 // TODO: get field data from server
 const getFieldList = () => [
@@ -60,135 +25,51 @@ const getFieldList = () => [
 ];
 
 function JoinPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
-  const [url, setUrl] = useState('');
   const [field, setField] = useState([]);
-
   const [showPassword, setShowPassword] = useState(false);
   const [agreement, setAgreement] = useState(false);
-  const [submitTried, setSubmitTried] = useState(false);
 
   // TODO: 분야선택도 조건에 넣기
   const history = useHistory();
   const fieldList = getFieldList();
 
-  const passwordState = useMemo(() => {
-    // 영문, 숫자, 특수문자
-    // 조합: 2가지 이상 혼합 사용
-    // 최소 4자 이상
-    if (password.length === 0) return 'empty';
-    else if (!isPassword(password)) return 'invalid_form';
-    else if (password.length < 5) return 'invalid_len_short';
-    else return 'valid';
-  }, [password]);
+  const email = useInput({ type: 'email' });
+  const password = useInput({ type: 'password' });
+  const name = useInput({ type: 'name' });
+  const url = useInput({ type: 'url' });
 
-  const emailState = useMemo(() => {
-    if (email === '') return 'empty';
-    else return isEmail(email) ? 'valid' : 'invalid';
-  }, [email]);
-
-  const urlState = useMemo(() => {
-    const formedUrl = `http://${url}.kr`;
-    if (url.length === 0) return 'empty';
-    // else if (중복체크state) return 'invalid_dup';
-    else if (!isURL(formedUrl)) return 'invalid_form';
-    else if (url.length < 4) return 'invalid_len_short';
-    else if (url.length > 20) return 'invalid_len_long';
-    else return 'valid';
-  }, [url]);
-
-  const fieldState = useMemo(() => {
-    // if (field.length >= 1) return 'valid';
-    // else return 'invalid';
-    return 'valid';
-  }, [field]);
+  const isValid = useCallback((inputState) => inputState === 'ok', []);
 
   const agreementState = useMemo(() => {
-    if (agreement) return 'valid';
-    else return 'invalid';
+    if (!agreement) {
+      return '약관에 동의해주세요.';
+    }
+    return 'ok';
   }, [agreement]);
-
-  const nameState = useMemo(() => {
-    if (name.length === 0) return 'empty';
-    // else if (중복체크state) return 'invalid_dup';
-    else if (name.length > 15) return 'invalid_len_long';
-    else return 'valid';
-  }, [name]);
-
-  const isValid = useCallback((inputState) => inputState === 'valid', []);
 
   const disableSubmit = useMemo(() => {
     if (
-      // isValid(agreementState) &&
-      // isValid(fieldState) &&
-      isValid(passwordState) &&
-      isValid(emailState) &&
-      isValid(urlState) &&
-      isValid(nameState)
+      isValid(password.state) &&
+      isValid(email.state) &&
+      isValid(url.state) &&
+      isValid(name.state) &&
+      isValid(agreementState)
     ) {
       return false;
     } else return true;
-  }, [
-    agreementState,
-    passwordState,
-    fieldState,
-    emailState,
-    urlState,
-    nameState,
-  ]);
-
-  const emailGuide = useMemo(() => {
-    if (emailState === 'invalid') return '잘못된 이메일 입력값';
-    else return '';
-  }, [emailState]);
-  const passwordGuide = useMemo(() => {
-    if (passwordState === 'invalid_form')
-      return '영문, 숫자, 특수문자 중 최소 2가지 조합으로 입력해주세요.';
-    else if (passwordState === 'invalid_len_short')
-      return '5글자 이상 입력해주세요.';
-    else return '';
-  }, [passwordState]);
-  // const fieldGuide = useMemo(() => {
-  //   if (fieldState === 'invalid' && submitTried)
-  //     return '관심분야를 하나 이상 선택해주세요.';
-  //   else return '';
-  // }, [fieldState, submitTried]);
-  const nameGuide = useMemo(() => {
-    if (nameState === 'invalid_dup') return '이미 사용중인 닉네임입니다.';
-    else if (nameState === 'invalid_len_long')
-      return '15자 이내로 입력해주세요.';
-    else return '';
-  }, [nameState]);
-  const urlGuide = useMemo(() => {
-    if (urlState === 'invalid_form')
-      return '언더스코어(_), 콜론(:), 공백문자( ), 슬래시(/)는 사용할 수 없습니다.';
-    else if (urlState === 'invalid_len_short')
-      return '4글자 이상 입력해주세요.';
-    else if (urlState === 'invalid_len_long')
-      return '20자 이내로 입력해주세요.';
-    else return '';
-  }, [urlState]);
-  const agreementGuide = useMemo(() => {
-    if (agreementState === 'invalid' && submitTried)
-      return '약관에 동의해주세요.';
-    else return '';
-  }, [agreementState, submitTried]);
+  }, [password.state, email.state, name.state, url.state, agreementState]);
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
-    if (agreementState === 'invalid') {
-      setSubmitTried(true);
-    } else {
-      postData({
-        nickname: name,
-        email,
-        password,
-        url,
-        field,
-      });
-    }
+    console.log('postData');
+    console.log(name, email, password, url, field);
+    // postData({
+    //   nickname: name,
+    //   // email,
+    //   password,
+    //   url,
+    //   field,
+    // });
   };
 
   const onFieldChange = useCallback(
@@ -218,121 +99,106 @@ function JoinPage() {
   );
 
   return (
-    <div css={Wrapper}>
-      <div css={Container}>
-        <div css={PageInfos}>
-          <div>
-            <button type='button' onClick={() => history.goBack()}>
-              되돌아가기 버튼
-            </button>
-            <span>
-              새 계정 생성을 위한 <wbr />
-              정보를 입력해
-              <wbr />
-              주세요.
-            </span>
-          </div>
-          <h2>Onit</h2>
+    <div css={Container}>
+      <div css={PageInfos}>
+        <div>
+          <button type='button' onClick={() => history.goBack()}>
+            되돌아가기 버튼
+          </button>
+          <span>
+            새 계정 생성을 위한 <wbr />
+            정보를 입력해
+            <wbr />
+            주세요.
+          </span>
         </div>
-        <form css={[InputList]} onSubmit={onSubmitHandler}>
-          <div css={[InputItem]} id='narrow'>
-            <label htmlFor='email'>이메일</label>
-            <div css={InputItemContents}>
-              <div css={Content}>
-                <input
-                  id='email'
-                  type='email'
-                  value={email}
-                  onChange={(event) => setEmail(event.currentTarget.value)}
-                />
-              </div>
-              <p>{emailGuide}</p>
-            </div>
-          </div>
-          <div css={[InputItem]} id='narrow'>
-            <label htmlFor='password'>비밀번호</label>
-            <div css={InputItemContents}>
-              <div css={Content}>
-                <div id='passwordDiv'>
-                  <input
-                    id='password'
-                    type={showPassword ? 'text' : 'password'}
-                    value={password}
-                    onChange={(event) => setPassword(event.currentTarget.value)}
-                  />
-                  <button
-                    id='showPasswordBtn'
-                    type='button'
-                    onClick={() => setShowPassword(!showPassword)}
-                  >
-                    {showPassword ? '비밀번호 숨기기' : '비밀번호 보이기'}
-                  </button>
-                </div>
-              </div>
-
-              <p>{passwordGuide}</p>
-            </div>
-          </div>
-          <div css={[InputItem]} id='narrow'>
-            <label htmlFor='nickname'>닉네임</label>
-            <div css={InputItemContents}>
-              <div css={Content}>
-                <input
-                  id='nickname'
-                  type='text'
-                  value={name}
-                  onChange={(event) => setName(event.currentTarget.value)}
-                />
-              </div>
-              <p>{nameGuide}</p>
-            </div>
-          </div>
-          <div css={[InputItem]} id='narrow'>
-            <label htmlFor='url'>개인 url</label>
-            <div css={InputItemContents}>
-              <div css={Content}>
-                <div id='urlDiv'>
-                  <p
-                    style={{
-                      margin: '6px',
-                    }}
-                  >
-                    iamonit.kr/
-                  </p>
-                  <input
-                    id='url'
-                    type='text'
-                    value={url}
-                    onChange={(event) => setUrl(event.currentTarget.value)}
-                    // placeholder={url}
-                  />
-                </div>
-              </div>
-              <p>{urlGuide}</p>
-            </div>
-          </div>
-          <div css={[InputItem]} id='wide'>
-            <label htmlFor='field'>분야 선택</label>
-            <div css={InputItemContents}>
-              <div css={Content}>{fieldButtons}</div>
-              {/* <p>{fieldGuide}</p> */}
-            </div>
-          </div>
-          <div css={InputConfirm}>
-            <input
-              type='checkbox'
-              id='agreement'
-              onChange={(event) => setAgreement(event.target.checked)}
-            />
-            <label htmlFor='agreement'>약관에 동의합니다.</label>
-
-            <p>{agreementGuide}</p>
-            <button type='submit' disabled={disableSubmit}>
-              생성 완료
-            </button>
-          </div>
-        </form>
+        <h2>Onit</h2>
       </div>
+      <form css={[InputList]} onSubmit={onSubmitHandler}>
+        <div css={[InputItem]} id='narrow'>
+          {' '}
+          <label htmlFor='email'>이메일</label>
+          <div css={InputItemContents}>
+            <div css={Content}>
+              <input id='email' type='email' {...email.input} />
+            </div>
+            <p>{email.state === 'ok' ? '' : email.state}</p>
+          </div>
+        </div>
+
+        <div css={[InputItem]} id='narrow'>
+          <label htmlFor='password'>비밀번호</label>
+          <div css={InputItemContents}>
+            <div css={Content}>
+              <div id='passwordDiv'>
+                <input
+                  id='password'
+                  type={showPassword ? 'text' : 'password'}
+                  {...password.input}
+                />
+                <button
+                  id='showPasswordBtn'
+                  type='button'
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? '비밀번호 숨기기' : '비밀번호 보이기'}
+                </button>
+              </div>
+            </div>
+            <p>{password.state === 'ok' ? '' : password.state}</p>
+          </div>
+        </div>
+        <div css={[InputItem]} id='narrow'>
+          <label htmlFor='nickname'>닉네임</label>
+          <div css={InputItemContents}>
+            <div css={Content}>
+              <input id='nickname' type='text' {...name.input} />
+            </div>
+            <p>{name.state === 'ok' ? '' : name.state}</p>
+          </div>
+        </div>
+        <div css={[InputItem]} id='narrow'>
+          <label htmlFor='url'>개인 url</label>
+          <div css={InputItemContents}>
+            <div css={Content}>
+              <div id='urlDiv'>
+                <p
+                  style={{
+                    margin: '6px',
+                  }}
+                >
+                  iamonit.kr/
+                </p>
+                <input
+                  id='url'
+                  type='text'
+                  {...url.input}
+                  // placeholder={url}
+                />
+              </div>
+            </div>
+            <p>{url.state === 'ok' ? '' : url.state}</p>
+          </div>
+        </div>
+        <div css={[InputItem]} id='wide'>
+          <label htmlFor='field'>분야 선택</label>
+          <div css={InputItemContents}>
+            <div css={Content}>{fieldButtons}</div>
+          </div>
+        </div>
+        <div css={InputConfirm}>
+          <input
+            type='checkbox'
+            id='agreement'
+            onChange={(event) => setAgreement(event.target.checked)}
+          />
+          <label htmlFor='agreement'>약관에 동의합니다.</label>
+          <p>{agreementState === 'ok' ? '' : agreementState}</p>
+          <button type='submit' disabled={disableSubmit}>
+            생성 완료
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
@@ -365,11 +231,6 @@ const fieldButton = css`
     background-color: black;
     color: white;
   }
-`;
-
-const Wrapper = css`
-  width: 100vw;
-  height: 100vh;
 `;
 
 const Container = css`
@@ -450,8 +311,8 @@ const Content = css`
     background-color: inherit;
     width: 100%;
     border: 0;
-    outline: 0;
   }
+
   #showPasswordBtn {
     background-color: inherit;
     border: 0;
@@ -483,3 +344,18 @@ const InputConfirm = css`
     padding: 20px;
   }
 `;
+
+// const postData = (data) => {
+//   console.log('[submited]');
+//   console.log(data);
+//   // fetch(`http://localhost:8080/auth/join/local`, {
+//   //   method: 'POST',
+//   //   headers: {
+//   //     Accept: 'application/json',
+//   //     'Content-Type': 'application/json',
+//   //   },
+//   //   body: JSON.stringify(data),
+//   // }).then((response) => {
+//   //   console.log(response);
+//   // });
+// };
